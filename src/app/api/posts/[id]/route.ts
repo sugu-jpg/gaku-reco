21a6e854a2c8488ea8a29d0c4108093a1d8dab2c
sugu-jpg <taakunda@gmail.com>
2025-06-18 17:47:07 +0900

    修正４

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
 
   try {
